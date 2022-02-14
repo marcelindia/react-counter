3c89229b91feeed23cacf0ed3e339a69d4f9c2fd
Diana Marcelin <dianamarcelin@Dianas-MacBook-Air.local>
2022-02-14 11:47:23 -0500

    added button for iced-hot-coffee

--- src/coffees.js
+++ src/coffees.js
@@ -2,15 +2,19 @@ import React, { useState, useEffect } from "react";
 
 function Coffees() {
   const [coffeeList, setCoffeeList] = useState();
+  const [temp, setTemp] = useState("hot");
   useEffect(() => {
-    fetch("https://api.sampleapis.com/coffee/hot")
+    fetch(`https://api.sampleapis.com/coffee/${temp}`)
       .then((response) => response.json())
       .then((data) => setCoffeeList(data)) //when we get the coffee list, it will be the array
       .catch((err) => console.error(err));
-  }, []);
+  }, [temp]);
   return (
     <section className="coffee-container" id="coffees">
       <h2>Coffee Recipes</h2>
+      <button onClick={() => setTemp("hot")}>Hot</button>
+      &nbsp;
+      <button onClick={() => setTemp("iced")}>Iced</button>
       {!coffeeList ? (
         <h3>Loading...</h3>
       ) : (
